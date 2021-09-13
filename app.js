056b17f861845d32534b147edda8f344cfe62975
Poojaa <poojaakarthisanthi@gmail.com>
2021-09-14 03:24:22 +0530

    added mock server

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var btnTranslate = document.querySelector("#btnTranslate");
 var input = document.querySelector("#input");
+var output = document.querySelector("#output");
 
-function translate() {
-    console.log(input.value);
+var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
+
+function constructURL(text) {
+    return serverURL + "?text=" + text;
 }
+function doFetch(text) {
+    fetch( constructURL(text) )
+    .then(response => response.json() )
+    .then(json => output.innerText=json.contents.translated );
+}
+function translate() {
+
+    doFetch(input.value);
+    //output.innerText;
+}
+
 btnTranslate.addEventListener("click",translate);
