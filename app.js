81d510fa640a047cb7cb5e6a44c799f539222e33
Poojaa <poojaakarthisanthi@gmail.com>
2021-09-14 03:32:12 +0530

    error handler added

--- app.js
+++ app.js
@@ -2,20 +2,26 @@ var btnTranslate = document.querySelector("#btnTranslate");
 var input = document.querySelector("#input");
 var output = document.querySelector("#output");
 
+function errorHandler(error) {
+    console.log("Error Occured! ",error);
+    alert("Server Down! Try Again Later");
+}
+
 var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json";
 
 function constructURL(text) {
     return serverURL + "?text=" + text;
 }
+
 function doFetch(text) {
     fetch( constructURL(text) )
     .then(response => response.json() )
-    .then(json => output.innerText=json.contents.translated );
+    .then(json => output.innerText=json.contents.translated )
+    .catch(errorHandler);
 }
-function translate() {
 
+function translate() {
     doFetch(input.value);
-    //output.innerText;
 }
 
 btnTranslate.addEventListener("click",translate);
